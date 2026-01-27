1d56524854c78c13f4a976a1999ce9597db4dc71
Diego <d761017@hotmail.com>
2026-01-27 09:20:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutri-app-v1';
+const CACHE_NAME = 'nutri-app-v1.2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -39,3 +39,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
