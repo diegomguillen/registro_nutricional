2c4cc909dfcc9964ae1925014ef3075a9bb95c61
Diego <d761017@hotmail.com>
2026-02-03 19:18:22 +0100

    Update service worker cache version to v1.50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutri-app-v1.45';
+const CACHE_NAME = 'nutri-app-v1.50';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -41,6 +41,7 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
 
